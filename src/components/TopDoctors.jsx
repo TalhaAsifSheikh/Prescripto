11e329c8c50194bc3129ba2ca31775e6527fd82d
Talha Asif <talhasifsheikh@gmail.com>
2024-09-15 02:29:00 -0700

    FrontEnd Completed

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -12,7 +12,7 @@ const TopDoctors = () => {
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10 '>
         <h1 className='text-3xl font-medium'>Top Doctors To Book</h1>
         <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors </p>
-        <div className='w-full grid grid-cols-5 gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
+        <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
                    
                 {doctors.slice(0,10).map((item,index)=>(
                     <div onClick={()=>{navigate(`/appointment/${item._id}`); scrollTo(0,0) }} className='border border-blue-200 rounded-x1 overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}> 
